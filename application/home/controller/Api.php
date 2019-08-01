2ce326fb20070daef02945d04cd41967aa340525
fuchushuimian <1223317525@qq.com>
2019-08-01 11:17:32 +0800

    主动选择BUG修复

--- application/home/controller/Api.php
+++ application/home/controller/Api.php
@@ -20,7 +20,7 @@ class Api extends ApiBase
         // update wp_room set `status` = 0;
         // truncate wp_waite ;
         // truncate wp_calls;
-        echo '请登录';
+        dump($_SESSION);
         exit;
     }
 
