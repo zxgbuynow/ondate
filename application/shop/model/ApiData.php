123a56026771d0656b232586037daf2a545e6448
fuchushuimian <1223317525@qq.com>
2019-08-22 11:37:03 +0800

    app

--- application/shop/model/ApiData.php
+++ application/shop/model/ApiData.php
@@ -1840,7 +1840,7 @@ class ApiData extends ApiBase
         //$where['room']=$room;
         $where['status']=1;
         //$where['type']=0;
-        $where['retime']='';
+        $where['retime']=null;
         $re=M('calls')->where($where)->find();
         if(!$re){
             return $this->error('亲，请扫码上钟！');
