c22928bae27d0633b3a23a2a86b618df0c40fe1e
fuchushuimian <1223317525@qq.com>
2019-09-20 09:34:37 +0800

    app

--- public/static/lodopFuncs.js
+++ public/static/lodopFuncs.js
@@ -48,8 +48,8 @@ function needCLodop() {
 
 //====页面引用CLodop云打印必须的JS文件,用双端口(8000和18000）避免其中某个被占用：====
 if (needCLodop()) {
-    var src1 = "http://localhost:8000/CLodopfuncs.js?priority=1";
-    var src2 = "http://localhost:18000/CLodopfuncs.js?priority=0";
+    var src1 = "__STATIC__/CLodopfuncs.js?priority=1";
+    var src2 = "__STATIC__/CLodopfuncs.js?priority=0";
 
     var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     var oscript = document.createElement("script");
