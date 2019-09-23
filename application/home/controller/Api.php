56b5e7660c01be80d78cfc5d6fed4d7162d1c55a
fuchushuimian <1223317525@qq.com>
2019-09-23 11:57:50 +0800

    app

--- application/home/controller/Api.php
+++ application/home/controller/Api.php
@@ -74,7 +74,7 @@ class Api extends ApiBase
     {
     	//$user = M('user')->find();
     	//session('userinfo',$user);
-    	$login=$_SESSION;
+    	$login=$_COOKIE;
     	dump($login);
 
 /*        //查找用户并插入队列
